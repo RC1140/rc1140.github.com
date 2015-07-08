274c7dc260708297adc5ef2183fc2746077a47ac
Jameel Haffejee <jameel@chasesoftware.co.za>
2015-07-08 07:41:42 +0200

    Added parsers and missing js refs

--- wiki/weight.js
+++ wiki/weight.js
@@ -1,8 +1,19 @@
 'use strict';
 $(document).ready(function() {
-    //var split_by_data;
+    
     d3.json('https://spreadsheets.google.com/feeds/list/1ctzBIkxtY89RH45ArIFLVwxLv50kvAI6eRunhCXt7v4/2/public/basic?alt=json', function(data) {
-        data = convert_dates(data, 'year');
+		var rows = data.feed.entry;
+		var rowObjects = $(rows).map(function(index,row){
+			var mappedRow = {};
+			mappedRow['Date'] = new Date(row.title.$t);
+			$.each(row.content.$t.split(','),function(i,val){
+				var mappedParts = val.trim().split(':');
+				mappedRow[mappedParts[0]] = parseInt(mappedParts[1].trim());
+			});
+			return mappedRow;
+		});
+		console.log(rowObjects);
+        //data = convert_dates(rows, 'year');
         data_graphic({
             title: "Weight over time",
             data: data,
@@ -10,8 +21,9 @@ $(document).ready(function() {
             chart_type: 'line',
             height: 200,
             target: '#weightHolder',
-            x_accessor: 'year',
-            y_accessor: 'count'
+            x_accessor: 'Date',
+            y_accessor: 'weight'
         });
+		
     });
 }, false);
--- wiki/weight.md
+++ wiki/weight.md
@@ -6,5 +6,8 @@ title: Weight Stats
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 <script src="/wiki/d3.min.js"></script>
 <script src="/wiki/weight.js"></script>
+<script src="/wiki/metricsgraphics.min.js"></script>
+<link rel="stylesheet" href="/wiki/metricsgraphics.css" type="text/css"></link>
+<link rel="stylesheet" href="/wiki/metricsgraphics-demo.css" type="text/css"></link>
 
 <div id="weightHolder"></div>
